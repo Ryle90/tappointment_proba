2ca41159848c8e0e4cb70ac4c2586a0c68a480a2
Tábi István <tabi.pisti@gmail.com>
2021-11-29 19:48:20 +0100

    created react app basic

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import logger from './logger.js';
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   logger.info(`App is listening on ${PORT}`);
