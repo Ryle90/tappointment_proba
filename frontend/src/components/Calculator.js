444fd2ef21b4ac3428d7dcc2cba14ae46bd42f8d
Tábi István <tabi.pisti@gmail.com>
2021-12-01 20:35:49 +0100

    fixed bugs and added keyboard-control

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -1,6 +1,6 @@
-import { Container } from "react-bootstrap";
+import { Container } from "react-bootstrap"
+import useEventListener from "@use-it/event-listener";
 import { useState } from "react";
-
 import calculateSolution from "../utils/calculateSolution";
 import { memoryEndpoint } from "../utils/endpoint";
 import requestToBackend from "../utils/requestToBackend";
@@ -17,8 +17,61 @@ export default function Calculator () {
     const [actualNumber, setActualNumber] = useState('0');
     const [otherMessage, setOtherMessage] = useState('');
 
-    function typeActualNumber (event) {        
-        const value = event.target.value;
+    const [isMKeyPressingDown, setIsMKeyPressingDown] = useState(false);
+
+    useEventListener('keydown', (event) => {
+        const numberKeyArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ','];
+        const operationKeyArray = ['/', '*', '-', '+'];
+        const key = event.key.toLowerCase();
+
+        if (numberKeyArray.includes(key)) {
+            //You can use comma instead of decimal point
+            key === ',' ? typeActualNumber('.') : typeActualNumber(key);
+        }
+
+        if (operationKeyArray.includes(key)) {
+            handleOperationButtonsClick(key);
+        }
+
+        if (key === 'enter') {
+            handleOperationButtonsClick('=');
+        }
+
+        if (key === 'backspace') {
+            cancelLastNumber();
+        }
+
+        if (key === 'delete') {
+            clearDisplay();
+        }
+
+        if (key === 'm') {
+            //Key combination: When you're keeping 'm' pressed, you can press other keys to use memory functions
+            setIsMKeyPressingDown(true);
+        }
+
+        if (key === 'r' && isMKeyPressingDown) {
+            getNumber();
+        }
+
+        if (key === 's' && isMKeyPressingDown) {
+            handleSaveNumber();
+        }
+
+        if (key === 'c') {
+            deleteNumber();
+        }
+
+    })
+
+    useEventListener('keyup', (event) => {
+        if (event.key.toLowerCase() === 'm') {
+            setIsMKeyPressingDown(false);
+        }
+    })
+
+    function typeActualNumber (value) {
+        //Actual number is the number you can see on the display of the calculator
         let tempActualNumber = actualNumber;
         let newActualNumber;
 
@@ -28,13 +81,20 @@ export default function Calculator () {
 
         if (value !== '.') {         
             if (actualNumber === '0') {
+                //This solution provides that cannot be typing more than one '0',
+                //if there isn't other number or decimal point on display
                 newActualNumber = value;
             } else {
                 newActualNumber = tempActualNumber + value;
             }
         } else {
+            //Cannot be more than one decimal point in actual number
             if (!actualNumber.toString().includes('.')) {
+                //When last click was click of an operation button (e.g +, *) actually number is not an empty string,
+                //but in this case we can type a new number on display
+                //(Temp actual number is empty string only if last click was click of an operation button)
                 if (tempActualNumber === '') {
+                    //If missing '0' before decimal point, will be displayed automatically
                     tempActualNumber = '0';
                 }
                 newActualNumber = tempActualNumber + value;
@@ -43,6 +103,7 @@ export default function Calculator () {
             }
         }
 
+        //Number of characters is limited
         if (newActualNumber.length <= 24) {
             setActualNumber(newActualNumber);
         }
@@ -87,14 +148,15 @@ export default function Calculator () {
         setOtherMessage('');
     }
 
-    function handleOperationButtonsClick (event) {
-        const value = event.target.value;
+    function handleOperationButtonsClick (value) {
         let newSequenceArray = JSON.parse(JSON.stringify(datasToCalculate.sequenceOfOperation));
         let isLastClickEquation = datasToCalculate.isLastClickEquation;
         let isOperationSymbolChange = false;
         let lastOperationSymbol;
 
         /*eslint-disable*/
+        //Sequence of operation is stored in an array. When you click an operation button,
+        //the value of actual number and symbol of operation button will be pushed into the array
         if (value !== '=' && datasToCalculate.isLastClickOperation === false || isLastClickEquation) {
             newSequenceArray.push(actualNumber);
             newSequenceArray.push(value);
@@ -102,7 +164,9 @@ export default function Calculator () {
             if (isLastClickEquation) {
                 isOperationSymbolChange = true;
             }
-
+        
+        //If you already pressed once '+', '-', '*', or '/' buttons,
+        //the last operation symbol in the array will be changed to the new one
         } else if (value !== '=') {
             newSequenceArray.pop();
             newSequenceArray.push(value);
@@ -111,6 +175,7 @@ export default function Calculator () {
         /*eslint-enable*/
 
         if(value === '=') {
+            //Only pressing of equality sign button will make empty the array of sequence of operation
             newSequenceArray = [];
             isLastClickEquation = true;
             lastOperationSymbol = ''
@@ -118,6 +183,8 @@ export default function Calculator () {
             lastOperationSymbol = value;
         }
         
+        //Every time when you have sequence of operation and you make a new operation
+        //the entire sequence of operations is evaluated
         if(!isOperationSymbolChange) {
             if (value === '=' || datasToCalculate.sequenceOfOperation.length !== 0) {
                 const sequenceOfSolution = JSON.parse(JSON.stringify(datasToCalculate.sequenceOfOperation));
@@ -230,32 +297,128 @@ export default function Calculator () {
                         <div className="number-decimal-button-group">
                             <div className="number-button-group">
                                 <div className="n7-n9-button-group">
-                                    <button value="7" className="btn btn-primary" onClick={typeActualNumber}>7</button>
-                                    <button value="8" className="btn btn-primary" onClick={typeActualNumber}>8</button>
-                                    <button value="9" className="btn btn-primary" onClick={typeActualNumber}>9</button>
+                                    <button
+                                        value="7" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        7
+                                    </button>
+                                    <button 
+                                        value="8" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        8
+                                    </button>
+                                    <button 
+                                        value="9" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        9
+                                    </button>
                                 </div>
                                 <div className="n4-n6-button-group">
-                                    <button value="4" className="btn btn-primary" onClick={typeActualNumber}>4</button>
-                                    <button value="5" className="btn btn-primary" onClick={typeActualNumber}>5</button>
-                                    <button value="6" className="btn btn-primary" onClick={typeActualNumber}>6</button>
+                                    <button 
+                                        value="4" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        4
+                                    </button>
+                                    <button 
+                                        value="5" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        5
+                                    </button>
+                                    <button 
+                                        value="6" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        6
+                                    </button>
                                 </div>
                                 <div className="n1-n3-button-group">
-                                    <button value="1" className="btn btn-primary" onClick={typeActualNumber}>1</button>
-                                    <button value="2" className="btn btn-primary" onClick={typeActualNumber}>2</button>
-                                    <button value="3" className="btn btn-primary" onClick={typeActualNumber}>3</button>
+                                    <button 
+                                        value="1" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        1
+                                    </button>
+                                    <button 
+                                        value="2" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        2
+                                    </button>
+                                    <button 
+                                        value="3" 
+                                        className="btn btn-primary" 
+                                        onClick={(event) => typeActualNumber(event.target.value)}
+                                    >
+                                        3
+                                    </button>
                                 </div>
                             </div>
                             <div className="other-button-group">
-                                <button value="0" className="btn btn-primary" onClick={typeActualNumber}>0</button>
-                                <button value="." className="btn btn-primary" onClick={typeActualNumber}>.</button>
+                                <button 
+                                    value="0" 
+                                    className="btn btn-primary" 
+                                    onClick={(event) => typeActualNumber(event.target.value)}
+                                >
+                                    0
+                                </button>
+                                <button
+                                    value="." 
+                                    className="btn btn-primary" 
+                                    onClick={(event) => typeActualNumber(event.target.value)}
+                                >
+                                    .
+                                </button>
                             </div>
                         </div>
                         <div className="operator-button-group">
-                            <button value="/" className="btn btn-primary" onClick={handleOperationButtonsClick}>/</button>
-                            <button value="*" className="btn btn-primary" onClick={handleOperationButtonsClick}>*</button>
-                            <button value="-" className="btn btn-primary" onClick={handleOperationButtonsClick}>-</button>
-                            <button value="+" className="btn btn-primary" onClick={handleOperationButtonsClick}>+</button>
-                            <button value="=" className="btn btn-danger" onClick={handleOperationButtonsClick}>=</button>
+                            <button 
+                                value="/" 
+                                className="btn btn-primary" 
+                                onClick={(event) => handleOperationButtonsClick(event.target.value)}
+                            >
+                                /
+                            </button>
+                            <button 
+                                value="*" 
+                                className="btn btn-primary" 
+                                onClick={(event) => handleOperationButtonsClick(event.target.value)}
+                            >
+                                *
+                            </button>
+                            <button 
+                                value="-" 
+                                className="btn btn-primary" 
+                                onClick={(event) => handleOperationButtonsClick(event.target.value)}
+                            >
+                                -
+                            </button>
+                            <button 
+                                value="+" 
+                                className="btn btn-primary" 
+                                onClick={(event) => handleOperationButtonsClick(event.target.value)}
+                            >
+                                +
+                            </button>
+                            <button
+                                value="=" 
+                                className="btn btn-danger" 
+                                onClick={(event) => handleOperationButtonsClick(event.target.value)}
+                            >
+                                =
+                            </button>
                         </div>
                     </div>
                 </div>
